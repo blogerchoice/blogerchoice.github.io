859a719304c468e25e00848b5e22ec0aac45a2fa
KAMARJAHAN <kamarjahankammu@gmail.com>
2024-08-09 19:28:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,13 @@
 $('a').on('click', function(event) {
   if (this.hash !== '') {
     event.preventDefault();
-    const hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800);
+    const hash = event.currentTarget.hash;
+    const targetElement = $(hash);
+    if (targetElement.length) {
+      $('html, body').animate({
+        scrollTop: targetElement.offset().top
+      }, 800);
+    }
   }
 });
 
